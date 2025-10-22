8a796a8a30118465367d3d68750c7c305c753767
oliveirajesuskauan-bit <oliveira.jesus.kauan@escola.pr.gov.br>
2025-10-22 08:37:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const avanca = document.querySelectorAll('.btn-proximo')
+
+avanca.forEach(button=>{
+     button.addEventListener('click', function{})
+     const atual = document,queryselector('ativo')
+     const proximoPasso = 'passo-' + this.getAttribute('data-proxino')
+
+
+     atual.classList.remove('ativo')
+     document.getElementById
+})
